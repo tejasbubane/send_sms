4d695f7182b64030286bcbbeaa138faaa0db4d0d
Tejas Bubane <tejasbubane@gmail.com>
2012-04-05 03:32:52 +0530

    first working copy

---
+++ sms.py
@@ -0,0 +1,17 @@
+# Copyright (C) 2012
+# Tejas Bubane (tejasbubane@gmail.com)
+# Date: 5 Apr 2012 # Time: 03:16
+
+import gammu
+
+sm = gammu.StateMachine()
+sm.ReadConfig()
+number = raw_input('Number: ')
+msg = raw_input('Message: ')
+sm.Init()
+message = {
+	'Text': str(msg),
+	'SMSC': {'Location': 1},
+	'Number': str(number),
+	}
+sm.SendSMS(message)
